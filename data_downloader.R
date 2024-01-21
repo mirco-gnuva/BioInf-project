d8cfc0dadc4923ac95ba2dec1540840cbfee403e
Mirco Gnuva <mirco.gnuva@gmail.com>
2024-01-21 12:54:25 +0100

    feat(data-downloader): Fallback to R for data download

---
+++ data_downloader.R
@@ -0,0 +1,53 @@
+# Select the first mirror for packages download
+chooseCRANmirror(ind=1)
+
+
+# Define requirements
+requirements <- c("bslib", "BiocManager", "SNFtool", "NetPreProc", "caret", "cluster", "mclustcomp")
+bioc_requirements <- c("curatedTCGAData", "TCGAutils", "TCGAbiolinks", "graph")
+
+
+# Install the requirements
+for (req in requirements){
+  if (!require(req, quietly = TRUE))
+    install.packages(req);
+}
+
+for (req in bioc_requirements){
+  if (!require(req, quietly = TRUE))
+    BiocManager::install(req);
+}
+
+
+# Import libraries
+library("curatedTCGAData");
+
+# Define download directory
+directory_path = './data'
+
+if (file.exists(directory_path)) {
+  print(paste("Directory", directory_path, "already exists."))
+} else {
+  print(paste("Creating", directory_path))
+  dir.create(directory_path, showWarnings = FALSE)
+  
+  # Check if the directory was created successfully
+  if (file.exists(directory_path)) {
+    print(paste("Directory", directory_path, "created successfully."))
+  } else {
+    print(paste("Failed to create directory", directory_path))
+  }
+}
+
+
+# Download the TCGA data
+assays <- c("miRNASeqGene", "RNASeq2Gene", "RPPAArray");
+mo <- curatedTCGAData(diseaseCode = "PRAD", 
+                      assays = assays, 
+                      version = "2.0.1", dry.run = FALSE);
+
+mo;
+
+
+# Export the downloaded data to 'data' folder
+exportClass(mo, dir = './data', fmt = "csv", ext = ".csv");
